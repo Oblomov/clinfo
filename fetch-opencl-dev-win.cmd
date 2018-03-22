06af380998dabbb2f25b0eab3436a0a0bfe91f17
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2018-03-22 21:44:24 +0100

    Improve Windows OpenCL dev fetcher
    Now it also accepts x64 as parameter to fetch 64-bit libs.

--- fetch-opencl-dev-win.cmd
+++ fetch-opencl-dev-win.cmd
@@ -1,7 +1,11 @@
-REM call as fetch-opencl-dev-win x86|x86_64
+REM call as fetch-opencl-dev-win x86|x86_64|x64
 
 git clone https://github.com/KhronosGroup/OpenCL-Headers
 move OpenCL-Headers/opencl22 include
 
-mkdir lib\%1
-curl -L -o lib/%1/libOpenCL.a https://github.com/AMD-FirePro/SDK/raw/master/external/opencl-1.2/lib/%1/libOpenCL.a -o lib/%1/OpenCL.lib https://github.com/AMD-FirePro/SDK/raw/master/external/opencl-1.2/lib/%1/OpenCL.lib -o OpenCL.dll https://github.com/AMD-FirePro/SDK/raw/master/external/opencl-1.2/bin/%1/OpenCL.dll
+set sub=%1
+
+if /i "%sub%" == "x64" set sub=x86_64
+
+mkdir lib\%sub%
+curl -L -o lib/%sub%/libOpenCL.a https://github.com/AMD-FirePro/SDK/raw/master/external/opencl-1.2/lib/%sub%/libOpenCL.a -o lib/%sub%/OpenCL.lib https://github.com/AMD-FirePro/SDK/raw/master/external/opencl-1.2/lib/%sub%/OpenCL.lib -o OpenCL.dll https://github.com/AMD-FirePro/SDK/raw/master/external/opencl-1.2/bin/%sub%/OpenCL.dll
